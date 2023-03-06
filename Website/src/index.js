3c520d9d0d3f9b0f52c30370df880dafe8873272
RaZe <razeslothc@gmail.com>
2023-03-06 21:05:27 +0200

    fix: use ejs to actually render the html and css

---
+++ Website/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { glob } = require('glob');
+const path = require('path');
+
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+	res.render('pages/index');
+});
+
+console.log(path.resolve(__dirname, 'public'));
+
+app.listen(3000, () => {
+	console.log('Server running on port 3000');
+});
+
+
+/* function loadFiles() {
+	const files = glob.sync(path.resolve(__dirname, 'public/html/*.html'));
+	for (const file of files) {
+		const fileName = path.basename(file).replace('.html', '');
+		console.log(fileName);
+		app.get(`/${fileName}`, (req, res) => {
+			res.render(file);
+		});
+	}
+}
+ */
---
+++ Website/src/public/js/script.js
@@ -0,0 +1,75 @@
+function dragElement(elmnt) {
+	let pos1 = 0;
+	let pos2 = 0;
+	let pos3 = 0;
+	let pos4 = 0;
+	const file = elmnt[0]; // get the file element
+	const fileHeader = file.querySelector('.fileheader'); // get the fileheader element
+	let fileBackgroundColor = ''; // variable to store the file border color
+
+	console.log(file.classList.item(0));
+
+	if (document.querySelector('.' + file.classList.item(0) + 'header')) {
+		// if present, the header is where you move the DIV from:
+		document.querySelector(
+			'.' + file.classList.item(0) + 'header',
+		).onmousedown = dragMouseDown;
+	} else {
+		// otherwise, move the DIV from anywhere inside the DIV:
+		fileHeader.onmousedown = dragMouseDown;
+	}
+
+	function dragMouseDown(e) {
+		e = e || window.event;
+		e.preventDefault();
+		// get the mouse cursor position at startup:
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+
+		// set the file border color to red
+		fileBackgroundColor = file.style.backgroundColor;
+		file.style.backgroundColor = 'rgba(211, 211, 211, 0.5)';
+
+		document.onmouseup = closeDragElement;
+		// call a function whenever the cursor moves:
+		document.onmousemove = elementDrag;
+	}
+
+	function elementDrag(e) {
+		e = e || window.event;
+		e.preventDefault();
+		// calculate the new cursor position:
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+
+		// calculate the new file position:
+		const newFileTop = file.offsetTop - pos2;
+		const newFileLeft = file.offsetLeft - pos1;
+
+		// check if the file is inside the screen
+		if (
+			newFileTop >= 0 &&
+      newFileLeft >= 0 &&
+      newFileTop + file.offsetHeight <= window.innerHeight &&
+      newFileLeft + file.offsetWidth <= window.innerWidth
+		) {
+			// set the file's new position:
+			file.style.top = newFileTop + 'px';
+			file.style.left = newFileLeft + 'px';
+		}
+	}
+
+	function closeDragElement() {
+		// reset the file border color
+		file.style.backgroundColor = fileBackgroundColor;
+
+		// stop moving when mouse button is released:
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
+}
+
+// Make the file element draggable:
+dragElement(document.getElementsByClassName('file'));
