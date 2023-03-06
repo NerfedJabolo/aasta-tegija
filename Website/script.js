891d1cfdb506ebbf22256a23a319af6311913524
NerfedJabolo <ezdlprofit1@gmail.com>
2023-03-06 20:48:09 +0200

    feat: changed background color on file drag. Also changed body background.

--- Website/script.js
+++ Website/script.js
@@ -1,14 +1,23 @@
-// Make the DIV element draggable:
-dragElement(document.getElementsByClassName('file')[0]);
-
 function dragElement(elmnt) {
   var pos1 = 0,
     pos2 = 0,
     pos3 = 0,
     pos4 = 0;
-  var fileHeader = elmnt.getElementsByClassName('fileheader')[0];
+  var file = elmnt[0]; // get the file element
+  var fileHeader = file.querySelector('.fileheader'); // get the fileheader element
+  var fileBackgroundColor = ''; // variable to store the file border color
 
-  fileHeader.onmousedown = dragMouseDown;
+  console.log(file.classList.item(0));
+
+  if (document.querySelector('.' + file.classList.item(0) + 'header')) {
+    // if present, the header is where you move the DIV from:
+    document.querySelector(
+      '.' + file.classList.item(0) + 'header'
+    ).onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    fileHeader.onmousedown = dragMouseDown;
+  }
 
   function dragMouseDown(e) {
     e = e || window.event;
@@ -16,6 +25,11 @@ function dragElement(elmnt) {
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
+
+    // set the file border color to red
+    fileBackgroundColor = file.style.backgroundColor;
+    file.style.backgroundColor = 'rgba(211, 211, 211, 0.5)';
+
     document.onmouseup = closeDragElement;
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
@@ -29,23 +43,33 @@ function dragElement(elmnt) {
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
-    var newX = elmnt.offsetLeft - pos1;
-    var newY = elmnt.offsetTop - pos2;
+
+    // calculate the new file position:
+    var newFileTop = file.offsetTop - pos2;
+    var newFileLeft = file.offsetLeft - pos1;
+
+    // check if the file is inside the screen
     if (
-      newX > 0 &&
-      newY > 0 &&
-      newX + elmnt.offsetWidth < window.innerWidth &&
-      newY + elmnt.offsetHeight < window.innerHeight
+      newFileTop >= 0 &&
+      newFileLeft >= 0 &&
+      newFileTop + file.offsetHeight <= window.innerHeight &&
+      newFileLeft + file.offsetWidth <= window.innerWidth
     ) {
-      elmnt.style.left = newX + 'px';
-      elmnt.style.top = newY + 'px';
+      // set the file's new position:
+      file.style.top = newFileTop + 'px';
+      file.style.left = newFileLeft + 'px';
     }
   }
 
   function closeDragElement() {
+    // reset the file border color
+    file.style.backgroundColor = fileBackgroundColor;
+
     // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
   }
 }
+
+// Make the file element draggable:
+dragElement(document.getElementsByClassName('file'));
